77ca90fbedb80af6d0d5168bb854a299e66cc6e8
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2023-07-09 09:37:37 +0330

    first-commit-of-neweet🐱‍🏍

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,25 @@
-import Image from 'next/image'
-import styles from './page.module.css'
 
+'use client'
+import { useEffect } from 'react'
 export default function Home() {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap")
+  },[])
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-3">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore deserunt fuga, expedita delectus consequuntur non.</p>
+        </div>
+        <div className="col-md-6">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab qui ad ea perferendis sit. Error consequatur perferendis autem nulla vero impedit. Libero, eligendi nihil voluptates incidunt iusto nostrum consequatur amet eius eos eveniet fuga accusantium! Repellendus sint quod suscipit ut ipsum. Nobis veritatis neque molestiae consectetur! Aperiam iste minima sequi, delectus veritatis maiores maxime dignissimos iure eos praesentium distinctio! Quasi?</p>
+        </div>
+        <div className="col-md-3">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In quam, ab hic recusandae at neque?</p>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
+    </>
   )
 }
