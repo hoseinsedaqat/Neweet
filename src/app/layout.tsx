686e90c8ec029e20fab99810c409b9342f791876
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2023-07-25 23:27:26 +0330

    fix/redux-not-working-on-production-mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
-'use client'
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import {store} from '@/store/index'
-import { Provider } from 'react-redux'
+import { Inter } from 'next/font/google'
+import { Providers } from '@/redux/reduxlayout'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,18 +13,18 @@ export const metadata: Metadata = {
   description: '(Twitter Clone Built by Hosein Sedaqat 2023)',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en" data-bs-theme="dark">
-      <body className={inter.className}>
-        <Provider store={store}>
-          {children}
-        </Provider>
-      </body>
-    </html>
-  )
-}
+export default function ReduxLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <html lang="en" data-bs-theme="dark">
+        <body className={inter.className}>
+          <Providers>
+            {children}
+          </Providers>
+        </body>
+      </html>
+    )
+  }
