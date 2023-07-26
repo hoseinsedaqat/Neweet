dca33717e04a47f6cad4be810725197ee2044fca
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2023-07-26 19:00:40 +0330

    add/preview-messages

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 import Sidebar from '@/components/layout/sidebar/sidebar'
+import HoseinSedaqat from '@/assets/HoseinSedaqat.jpg'
 import { useSelector } from 'react-redux'
 import { useEffect } from 'react'
+import Image from 'next/image'
 function Messages() {
     const test = useSelector<any, boolean>(state => state.tweet.tweet);
     console.log(test);
@@ -13,12 +15,150 @@ function Messages() {
         <div className="container">
           <div className="row">
             <Sidebar />
-            <div className="col-md-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur dicta totam rem praesentium voluptatibus quo consequatur quibusdam obcaecati saepe? Obcaecati aliquid placeat molestias, at minima reprehenderit voluptas, voluptatibus quae natus dolore enim consequuntur! Ut officia, fugit nihil ipsa ducimus aliquam, repudiandae aspernatur pariatur neque consequuntur eius beatae incidunt accusantium tenetur id atque iste voluptatibus molestiae! Possimus temporibus nemo necessitatibus quod eius quas, nisi illum dolorem eligendi, veniam rem labore sit, harum soluta itaque corrupti vel excepturi. Debitis exercitationem maiores in itaque, distinctio hic recusandae explicabo amet doloremque accusamus, laudantium architecto perferendis commodi molestias eligendi sapiente assumenda quo! Odit, hic inventore.
-            </div>
-            <div className="col-md-5">
+            <section className="col-md-4" id='messages-preview'>
+              <div className="messages-header">
+                <p className='fs-5'>
+                  Messages
+                </p>
+                <div>
+                  <p>
+                    <i className="bi bi-gear"></i>
+                  </p>
+                  <p>
+                    <i className="bi bi-envelope-plus"></i>
+                  </p>
+                </div>
+              </div>
+              <div className="messages-search">
+                <div>
+                  <i className="bi bi-search text-muted"></i>
+                </div>
+                <input type="text" placeholder='Search Direct Messages' className='text-muted'/>
+              </div>
+              <div className="messages-user">
+                <div>
+                  <Image src={HoseinSedaqat} alt='User-Profile-Img'/>
+                </div>
+                <div>
+                  <span>
+                    <small>Hosein</small>
+                    <small className='text-muted mx-1'>@HoseinSedaqat</small>
+                    <small className='text-muted'>Jul 8</small>
+                  </span>
+                  <span>
+                    <small className='text-muted'>This is my Awesome Message...</small>
+                  </span>
+                </div>
+              </div>
+              <div className="messages-user">
+                <div>
+                  <Image src={HoseinSedaqat} alt='User-Profile-Img'/>
+                </div>
+                <div>
+                  <span>
+                    <small>Hosein</small>
+                    <small className='text-muted mx-1'>@HoseinSedaqat</small>
+                    <small className='text-muted'>Jul 8</small>
+                  </span>
+                  <span>
+                    <small className='text-muted'>This is my Awesome Message...</small>
+                  </span>
+                </div>
+              </div>
+              <div className="messages-user">
+                <div>
+                  <Image src={HoseinSedaqat} alt='User-Profile-Img'/>
+                </div>
+                <div>
+                  <span>
+                    <small>Hosein</small>
+                    <small className='text-muted mx-1'>@HoseinSedaqat</small>
+                    <small className='text-muted'>Jul 8</small>
+                  </span>
+                  <span>
+                    <small className='text-muted'>This is my Awesome Message...</small>
+                  </span>
+                </div>
+              </div>
+              <div className="messages-user">
+                <div>
+                  <Image src={HoseinSedaqat} alt='User-Profile-Img'/>
+                </div>
+                <div>
+                  <span>
+                    <small>Hosein</small>
+                    <small className='text-muted mx-1'>@HoseinSedaqat</small>
+                    <small className='text-muted'>Jul 8</small>
+                  </span>
+                  <span>
+                    <small className='text-muted'>This is my Awesome Message...</small>
+                  </span>
+                </div>
+              </div>
+              <div className="messages-user">
+                <div>
+                  <Image src={HoseinSedaqat} alt='User-Profile-Img'/>
+                </div>
+                <div>
+                  <span>
+                    <small>Hosein</small>
+                    <small className='text-muted mx-1'>@HoseinSedaqat</small>
+                    <small className='text-muted'>Jul 8</small>
+                  </span>
+                  <span>
+                    <small className='text-muted'>This is my Awesome Message...</small>
+                  </span>
+                </div>
+              </div>
+              <div className="messages-user">
+                <div>
+                  <Image src={HoseinSedaqat} alt='User-Profile-Img'/>
+                </div>
+                <div>
+                  <span>
+                    <small>Hosein</small>
+                    <small className='text-muted mx-1'>@HoseinSedaqat</small>
+                    <small className='text-muted'>Jul 8</small>
+                  </span>
+                  <span>
+                    <small className='text-muted'>This is my Awesome Message...</small>
+                  </span>
+                </div>
+              </div>
+              <div className="messages-user">
+                <div>
+                  <Image src={HoseinSedaqat} alt='User-Profile-Img'/>
+                </div>
+                <div>
+                  <span>
+                    <small>Hosein</small>
+                    <small className='text-muted mx-1'>@HoseinSedaqat</small>
+                    <small className='text-muted'>Jul 8</small>
+                  </span>
+                  <span>
+                    <small className='text-muted'>This is my Awesome Message...</small>
+                  </span>
+                </div>
+              </div>
+              <div className="messages-user">
+                <div>
+                  <Image src={HoseinSedaqat} alt='User-Profile-Img'/>
+                </div>
+                <div>
+                  <span>
+                    <small>Hosein</small>
+                    <small className='text-muted mx-1'>@HoseinSedaqat</small>
+                    <small className='text-muted'>Jul 8</small>
+                  </span>
+                  <span>
+                    <small className='text-muted'>This is my Awesome Message...</small>
+                  </span>
+                </div>
+              </div>
+            </section>
+            <section className="col-md-5">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque ad totam nulla eius corporis adipisci, officiis tenetur, repudiandae nesciunt, sint itaque earum consectetur ullam maiores voluptas. Rerum fugit soluta aspernatur ducimus harum quia numquam reiciendis in qui? Reprehenderit autem facilis dolorem nisi. Ut, tenetur magnam! Soluta magni eius reprehenderit id tempore error illo ut porro? Corporis recusandae minima, id dicta veniam saepe sapiente! Consequuntur similique eveniet dicta dignissimos optio fugit consequatur, deserunt, ipsum eum amet reiciendis voluptatibus vitae aperiam sit nobis, veritatis illum ab omnis aspernatur id cupiditate debitis? Corporis, voluptatum. Temporibus cumque dolorem quibusdam cum ipsum ratione dolores ad!
-            </div>
+            </section>
            </div>
         </div> 
         </>
