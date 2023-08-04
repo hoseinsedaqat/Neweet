24ce50585e1cb074c9dce9a29d7ab82b64e2f64f
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2023-08-04 11:32:54 +0330

    add/not-found-view

---
+++ src/app/not-found.tsx
@@ -0,0 +1,32 @@
+'use client'
+import Sidebar from "@/components/layout/sidebar/sidebar";
+import SidebarMd from "@/components/layout/sidebar/sidebarmd";
+import Link from "next/link";
+import { useEffect } from 'react'
+function NotFound() {
+    useEffect(() => {
+        require("bootstrap/dist/js/bootstrap")
+      },[])
+    return ( 
+        <>
+            <div className="container">
+                <div className="row">
+                    <Sidebar />
+                    <section className="col-md-9" id="not-found">
+                        <SidebarMd id = ''/>
+                        <main>
+                            <p>
+                                {`Hmm...this page doesn't exist. Try searchingfor something else.`} 
+                            </p>
+                            <Link href={'/'} className="bg-primary">
+                                Search
+                            </Link>
+                        </main>
+                    </section>
+                </div>
+            </div>
+        </>
+     );
+}
+
+export default NotFound;
