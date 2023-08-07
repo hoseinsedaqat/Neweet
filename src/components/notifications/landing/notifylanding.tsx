856cefb329721d6d9a0b04ebc3b56a81f1c9168c
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2023-08-07 15:08:37 +0330

    update/notification-landing-data

--- src/components/notifications/landing/notifylanding.tsx
+++ src/components/notifications/landing/notifylanding.tsx
@@ -1,111 +1,51 @@
-import Image from 'next/image'
-import HoseinSedaqat from '@/assets/HoseinSedaqat.jpg'
-import SidebarMd from '@/components/layout/sidebar/sidebarmd'
+import SidebarMd from "@/components/layout/sidebar/sidebarmd";
+
+import { notification_data } from "@/data/notification";
+
+import Image from "next/image";
+
 function NotifyLanding() {
-    return ( 
-        <>
-            <section className="col-md-5" id="notifications">
-                <SidebarMd id=''/>
-                <main>
-                    <div className="notifications-header">
-                        <h5>Notifications</h5>
-                        <div className="cursor-pointer">
-                            <i className="bi bi-gear"></i>
-                        </div>
-                    </div>
-                    <div className="notifications-topics">
-                        <p>All</p>
-                        <p className='text-muted'>Verified</p>
-                        <p className='text-muted'>Mentions</p>
-                    </div>
-                    <div className="notifications-content">
-                        <div>
-                            <Image src={HoseinSedaqat} alt='User-Profile-Img'/>
-                            <p className='cursor-pointer'>...</p>
-                        </div>
-                        <div>
-                            <small>Hosein Sedaqat</small>
-                        </div>
-                        <div>
-                            <small className='text-muted'>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum nostrum in molestias amet ab quis unde tempora dicta at. Iure ea, necessitatibus quo nostrum excepturi reiciendis fugiat quisquam! Eius, omnis.
-                            </small>
-                        </div>
-                    </div>
-                    <div className="notifications-content">
-                        <div>
-                            <Image src={HoseinSedaqat} alt='User-Profile-Img'/>
-                            <p className='cursor-pointer'>...</p>
-                        </div>
-                        <div>
-                            <small>Hosein Sedaqat</small>
-                        </div>
-                        <div>
-                            <small className='text-muted'>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum nostrum in molestias amet ab quis unde tempora dicta at. Iure ea, necessitatibus quo nostrum excepturi reiciendis fugiat quisquam! Eius, omnis.
-                            </small>
-                        </div>
-                    </div>
-                    <div className="notifications-content">
-                        <div>
-                            <Image src={HoseinSedaqat} alt='User-Profile-Img'/>
-                            <p className='cursor-pointer'>...</p>
-                        </div>
-                        <div>
-                            <small>Hosein Sedaqat</small>
-                        </div>
-                        <div>
-                            <small className='text-muted'>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum nostrum in molestias amet ab quis unde tempora dicta at. Iure ea, necessitatibus quo nostrum excepturi reiciendis fugiat quisquam! Eius, omnis.
-                            </small>
-                        </div>
-                    </div>
-                    <div className="notifications-content">
-                        <div>
-                            <Image src={HoseinSedaqat} alt='User-Profile-Img'/>
-                            <p className='cursor-pointer'>...</p>
-                        </div>
-                        <div>
-                            <small>Hosein Sedaqat</small>
-                        </div>
-                        <div>
-                            <small className='text-muted'>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum nostrum in molestias amet ab quis unde tempora dicta at. Iure ea, necessitatibus quo nostrum excepturi reiciendis fugiat quisquam! Eius, omnis.
-                            </small>
-                        </div>
-                    </div>
-                    <div className="notifications-content">
-                        <div>
-                            <Image src={HoseinSedaqat} alt='User-Profile-Img'/>
-                            <p className='cursor-pointer'>...</p>
-                        </div>
-                        <div>
-                            <small>Hosein Sedaqat</small>
-                        </div>
-                        <div>
-                            <small className='text-muted'>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum nostrum in molestias amet ab quis unde tempora dicta at. Iure ea, necessitatibus quo nostrum excepturi reiciendis fugiat quisquam! Eius, omnis.
-                            </small>
-                        </div>
-                    </div>
-                    <div className="notifications-content">
-                        <div>
-                            <Image src={HoseinSedaqat} alt='User-Profile-Img'/>
-                            <p className='cursor-pointer'>...</p>
-                        </div>
-                        <div>
-                            <small>Hosein Sedaqat</small>
-                        </div>
-                        <div>
-                            <small className='text-muted'>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum nostrum in molestias amet ab quis unde tempora dicta at. Iure ea, necessitatibus quo nostrum excepturi reiciendis fugiat quisquam! Eius, omnis.
-                            </small>
-                        </div>
-                    </div>
-                </main>
-            </section>
-        </>
-     );
+  return (
+    <>
+      <section
+        className='col-md-5'
+        id='notifications'>
+        <SidebarMd id='' />
+        <main>
+          <div className='notifications-header'>
+            <h5>Notifications</h5>
+            <div className='cursor-pointer'>
+              <i className='bi bi-gear'></i>
+            </div>
+          </div>
+          <div className='notifications-topics'>
+            <p>All</p>
+            <p className='text-muted'>Verified</p>
+            <p className='text-muted'>Mentions</p>
+          </div>
+          {notification_data.map((user, idx) => (
+            <div
+              className='notifications-content'
+              key={idx}>
+              <div>
+                <Image
+                  src={user.img}
+                  alt='User-Profile-Img'
+                />
+                <p className='cursor-pointer'>...</p>
+              </div>
+              <div>
+                <small>{user.name}</small>
+              </div>
+              <div>
+                <small className='text-muted'>{user.text}</small>
+              </div>
+            </div>
+          ))}
+        </main>
+      </section>
+    </>
+  );
 }
 
 export default NotifyLanding;
---
+++ src/data/notification.ts
@@ -0,0 +1,44 @@
+import NazaninChekinpour from "@/assets/NazaninChekinpour.jpg";
+
+import AmirhassanAzimi from "@/assets/AmirhassanAzimi.jpg";
+
+import NedaDivbandi from "@/assets/NedaDivbandi.jpg";
+
+import SayehGhaderi from "@/assets/SayehGhaderi.jpg";
+
+import AliNazari from "@/assets/AliNazari.jpg";
+
+import ManuRink from "@/assets/ManuRink.jpg";
+
+export const notification_data = [
+  {
+    name: "Neda Divbandi",
+    img: NedaDivbandi,
+    text: `Quick six blind smart out burst. Perfectly on furniture dejection determine my depending an to. Add short water court fat. Her bachelor honoured perceive securing but desirous ham required.`,
+  },
+  {
+    name: "Amirhassan Azimi",
+    img: AmirhassanAzimi,
+    text: `Arrival entered an if drawing request. How daughters not promotion few knowledge contented.`,
+  },
+  {
+    name: "Ali Nazari",
+    img: AliNazari,
+    text: `Two exquisite objection delighted deficient yet its contained. Cordial because are account evident its subject but eat. Can properly followed learning prepared you doubtful yet him. Over many our good lady feet ask that`,
+  },
+  {
+    name: "Manu Rink",
+    img: ManuRink,
+    text: `To sorry world an at do spoil along. Incommode he depending do frankness remainder to. Edward day almost active him friend thirty piqued.`,
+  },
+  {
+    name: "Sayeh Ghaderi",
+    img: SayehGhaderi,
+    text: `Neat own nor she said see walk. And charm add green you these. Sang busy in this drew ye fine. At greater prepare musical so attacks as on distant`,
+  },
+  {
+    name: "Nazanin Chekinpour",
+    img: NazaninChekinpour,
+    text: `Carriage quitting securing be appetite it declared. High eyes kept so busy feel call in. Would day nor ask walls known.`,
+  },
+];
